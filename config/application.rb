284b4e3dc407974b815ea69c5d4c2b2a6ed68dab
chii220 <chisato.umeda@di-v.ne.jp>
2019-11-15 15:10:50 +0900

    master

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,10 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
-module ChatSpace2
+module ChatSpace3
 class Application < Rails::Application
   config.generators do |g|
     g.stylesheets false
@@ -14,5 +15,7 @@ class Application < Rails::Application
     g.helper false
     g.test_framework false
   end
+
+  config.i18n.default_locale = :ja
 end
 end
